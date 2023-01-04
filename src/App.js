16688f57392b0d784c3f609f135743005931d6cd
Papayusen <KarolekB2002@gmail.com>
2023-01-05 00:38:47 +0100

    Problem with state update

--- src/App.js
+++ src/App.js
@@ -2,17 +2,21 @@ import "./App.css";
 import Operation from "./components/Operation.jsx";
 import { useState, useEffect } from "react";
 function App() {
-  const [number1, setnumber1] = useState();
-  const [number2, setnumber2] = useState();
+  const [number1, setnumber1] = useState(0);
+  const [number2, setnumber2] = useState(0);
   const [order, setOrder] = useState([]);
   const [disp, setDisp] = useState("");
 
   const AssignNumber = (val) => {
     setDisp(disp + val);
+    console.log(disp);
   };
-  const AssignOperation = (val) => {
-    setnumber1(+disp);
-    setOrder(number1);
+  const AssignOperation = (num, val) => {
+    setnumber1(num);
+    setOrder(num);
+    setOrder(order.concat(val));
+    setDisp("");
+    console.log(number1 + " " + val);
   };
   return (
     <div className="App">
@@ -22,7 +26,7 @@ function App() {
           <Operation sign="1" function={() => AssignNumber(1)} />
           <Operation sign="2" function={() => AssignNumber(2)} />
           <Operation sign="3" function={() => AssignNumber(3)} />
-          <Operation sign="+" function={() => AssignOperation("+")} />
+          <Operation sign="+" function={() => AssignOperation(disp, "+")} />
         </div>
       </header>
     </div>
