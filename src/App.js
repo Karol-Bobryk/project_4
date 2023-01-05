a3536061173a99162a4e35aaa2b3040e95546e9f
Karol-Bobryk <120224663+Karol-Bobryk@users.noreply.github.com>
2023-01-05 14:36:35 +0200

    Async disp

--- src/App.js
+++ src/App.js
@@ -2,31 +2,33 @@ import "./App.css";
 import Operation from "./components/Operation.jsx";
 import { useState, useEffect } from "react";
 function App() {
-  const [number1, setnumber1] = useState(0);
-  const [number2, setnumber2] = useState(0);
+  const [number1, setNumber1] = useState(0);
+  const [number2, setNumber2] = useState(0);
   const [order, setOrder] = useState([]);
-  const [disp, setDisp] = useState("");
+  const [display, setDisplay] = useState("");
 
-  const AssignNumber = (val) => {
-    setDisp(disp + val);
-    console.log(disp);
-  };
+  function AssignNumber(val) {
+    setDisplay(display + val);
+  }
   const AssignOperation = (num, val) => {
-    setnumber1(num);
+    setNumber1(num);
     setOrder(num);
     setOrder(order.concat(val));
-    setDisp("");
-    console.log(number1 + " " + val);
+    setDisplay("");
   };
+  useEffect(() => {
+    console.log(display);
+  }, [display]);
   return (
     <div className="App">
       <header className="App-header">
-        {disp}
+        {display}
         <div>
           <Operation sign="1" function={() => AssignNumber(1)} />
           <Operation sign="2" function={() => AssignNumber(2)} />
           <Operation sign="3" function={() => AssignNumber(3)} />
-          <Operation sign="+" function={() => AssignOperation(disp, "+")} />
+          <Operation sign="+" function={() => AssignOperation(display, "+")} />
+          <Operation sign="=" function={() => AssignOperation(display, "+")} />
         </div>
       </header>
     </div>
